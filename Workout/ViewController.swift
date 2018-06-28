6565d36266c4d7680e1ff3ab4b44f5e6be1111b2
Brian Murphy <brian.murphy@vibes.com>
2018-06-28 14:40:19 -0500

    Add timer functionality to main screen

--- Workout/ViewController.swift
+++ Workout/ViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @objc func updateTimer() {
+        counter = counter + 0.1
+        timeLabel.text = String(format: "%.1f", counter)
+    }
+
+    @IBAction func startTimer(_ sender: Any) {
+        if(isPlaying) {
+            return
+        }
+        startButton.isEnabled = false
+        pauseButton.isEnabled = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        isPlaying = true
+    }
+    @IBAction func pauseTimer(_ sender: Any) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+    }
+    @IBAction func resetTImer(_ sender: Any) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+        timeLabel.text = String(counter)
+    }
+    
+    var counter = 0.0
+    var timer = Timer()
+    var isPlaying = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        timeLabel.text = String(counter)
+        pauseButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
