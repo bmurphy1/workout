de9e2ad0cb009b127ce2c0f6f682be80c9e0e4a2
Brian Murphy <brian.murphy@vibes.com>
2018-07-01 18:36:06 -0500

    Can play and pause music from user's Music Library

--- Workout/ViewController.swift
+++ Workout/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController {
 
@@ -15,13 +17,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     
+    @IBOutlet weak var playedTime: UILabel!
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var playOrPauseMusic: UIButton!
+    @IBOutlet weak var nextTrackButton: UIButton!
+    
     @objc func updateTimer() {
         counter = counter + 0.1
         timeLabel.text = String(format: "%.1f", counter)
     }
-
+    @objc func updatePlayingTimer() {
+        counter = counter + 0.1
+        playedTime.text = String(format: "%.1f", counter)
+        
+//        var currentTime = Int(audioPlayer.currentTime)
+//        var minutes = currentTime/60
+//        var seconds = currentTime - minutes * 60
+//
+//        playedTime.text = NSString(format: "%02d:%02d", minutes,seconds) as String
+    }
+    
+    var counter = 0.0
+    var timer = Timer()
+    var isRunning = false
+    
     @IBAction func startTimer(_ sender: Any) {
-        if(isPlaying) {
+        if(isRunning) {
             return
         }
         startButton.isEnabled = false
@@ -42,20 +63,43 @@ class ViewController: UIViewController {
         pauseButton.isEnabled = false
         
         timer.invalidate()
-        isPlaying = false
+        isRunning = false
         counter = 0.0
         timeLabel.text = String(counter)
     }
     
-    var counter = 0.0
-    var timer = Timer()
-    var isPlaying = false
+    @IBAction func playOrPauseMusicButton(_ sender: Any) {
+        if (isPlaying) {
+            musicPlayer.pause()
+            isPlaying = false
+        } else {
+            playerTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updatePlayingTimer), userInfo: nil, repeats: true)
+            musicPlayer.play()
+            isPlaying = true
+
+        }
+    }
     
+    @IBAction func nextTrack(_ sender: Any) {
+        print("isPlaying: \(isPlaying)")
+        if (isPlaying) {
+            musicPlayer.skipToNextItem()
+        } else {
+            print("no track playing")
+        }
+    }
+    
+    let musicPlayer = MPMusicPlayerApplicationController.applicationQueuePlayer
+    var isPlaying = false
+    var playerTimer:Timer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         timeLabel.text = String(counter)
         pauseButton.isEnabled = false
+        let musicQueue = MPMediaQuery.songs()
+        musicPlayer.setQueue(with: musicQueue)
     }
 
     override func didReceiveMemoryWarning() {
